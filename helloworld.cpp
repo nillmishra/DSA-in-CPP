8a285924986a64dd5d9928879058431b76e4cecb
Nill <nillmishra09@gmail.com>
2023-07-13 22:46:57 +0530

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello AEC";
+    cout <<"\n"; 
+    int a = 7;
+    cout << a << endl;
+    cout << "\n";
+    char b = 'A';
+    cout << b<< endl;
+
+    bool bl =true;
+    cout << bl << endl;
+
+    float f =2.4;
+    cout << f << endl;
+
+    double  d  = 8.909;
+    cout << d <<endl;
+
+    int size = sizeof(f);
+    cout << "size of f is: " << size << endl;
+    int v = 'a';
+    cout << v << endl;
+
+    char ch = 98;
+    cout << ch << endl;
+
+    char ch1= 123456;
+    cout << ch1 << endl;
+
+    unsigned int k = -122;
+    cout << k << endl;
+
+    int h = 5;
+    int y =7;
+    bool first = (h==y);
+    cout << first << endl;\
+
+    bool first1 = (h>=y);
+    cout << first1 << endl;
+
+    bool first2 = (h<=y);
+    cout << first2 << endl;
+}
