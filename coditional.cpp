2fbcf55ca551dfe6506754ae9f35e2876814f1c2
Nill <nillmishra09@gmail.com>
2023-07-13 22:47:26 +0530

    new file aded

---
+++ coditional.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*
+    int n;
+    cin >> n;
+
+    cout << "value of n is :" << n <<endl;
+    if (n >0)
+    cout << "+ve number" << endl;
+    else
+    cout << "-ve number";
+    */
+
+   /*
+   int a, b;
+   cout << "Enter the value of a : ";
+   cin >> a;
+   cout << "Enter the value of b : ";
+   cin >> b;
+   cout << "value of a & b is " << a << " & " << b << endl;
+
+   if (a > b)
+   {
+    cout << a <<" is greater than " << b << endl;
+   }
+   else{
+    cout << b <<" is greater than " << a << endl;
+   }
+   */
+
+  int a;
+  cout<< "Enter the value iof a : ";
+  cin >> a;
+  if (a> 0)
+  cout << "Ais positive";
+  else if (a == 0)
+  cout << "A is equal";
+  else 
+  cout << "A is negative";
+  
+    
+}
