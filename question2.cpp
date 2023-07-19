253d56d4cfe4805f8381a6d1a5cf50b97c6014e6
Nill <nillmishra09@gmail.com>
2023-07-20 00:02:27 +0530

    for Big O(n)

---
+++ question2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] ={1, 2, 3, 4, 5, 6, 7, 8,9};
+    int n =9;
+    for (int i =0; i < 9; i++ ){
+        cout <<arr[i] <<endl;
+    }
+    return 0; //O(n)
+}
