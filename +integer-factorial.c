7dea74fc6961f7bf3b243bd118575ce1215d1c19
Nill <nillmishra09@gmail.com>
2023-07-19 23:46:53 +0530

    addded some new code

---
+++ +integer-factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int fact(int );
+
+void main()
+{
+    int n, v;
+    puts("Enter any +intrger number :");
+    scanf("%d", &n);
+    v = fact(n);
+    printf("%d is the factorial value of %d",v , n);
+}
+int fact(int x){
+    if (x == 0 || x == 1)
+    return 1;
+    else
+    return(x*fact(x-1));
+}
+
---
+++ timecomplexity.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int ans = 0, y =9;
+    for (int i =5; i<=y; i++)
+    {
+        ans+=i;
+    }
+    cout << ans << endl;
+}
