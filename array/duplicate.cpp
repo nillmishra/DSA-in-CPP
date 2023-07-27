88077e3b3bc9a1caf13513c486730173c0f47e23
Nill <nillmishra09@gmail.com>
2023-07-27 19:47:56 +0530

    files in systematic

---
+++ array/duplicate.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, i;
+    cin >> n;
+    int arr[n];
+    for (i = 0; i < n; i++ ){
+        cin >> arr[i];
+    }
+    for (i = 0; i < n-1; i++ ){
+        cout << arr[i]<< " ";
+        }
+}
