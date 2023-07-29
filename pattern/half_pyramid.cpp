3a88a907ff5dedf59666b96ff943e2619660a9c2
Nill <nillmishra09@gmail.com>
2023-07-30 02:22:39 +0530

    2nd loop mai row +1 ho jayega

---
+++ pattern/half_pyramid.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int r =0; r < 5; r++){
+            for (int i =0; i < r + 1; i++){
+                cout << "* ";
+            }
+        cout <<endl;
+        }
+        
+    }
