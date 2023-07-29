76ecce5ecc7ca05fa35583af6723673820bf607c
Nill <nillmishra09@gmail.com>
2023-07-30 03:04:59 +0530

    full pyramid

---
+++ pattern/hollow_full_pyramid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >>n;
+    for(int r =0; r<n; r++){
+        int k =0;
+        for(int c =0; c<(2*n) - 1;c++){
+        if(c < n- r -1){
+            cout << " ";
+        }
+        else if(k< 2*r + 1){
+            cout <<"*";
+            k++;
+        }
+        else{
+            cout<<" ";
+        }
+        //cout <<endl;
+    }
+    cout <<endl;
+}
+}
