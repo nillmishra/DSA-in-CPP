47104850b85d36a3e99a39541b898ca3f0eef2e9
Nill <nillmishra09@gmail.com>
2023-07-30 13:28:38 +0530

    flip-space-triangle

---
+++ pattern/flip.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int r =0; r < n; r++){
+        for (int c=0;c <n -r; c++){
+            cout << "*";
+        }
+        for (int c =0; c < 2* r + 1; c++){ // row  + 1
+             cout << " ";
+        }
+        for (int c =0; c < n-r; c++){
+            cout << "*";
+        }
+        cout <<endl;
+        }
+
+        //half
+        for(int r =0; r < n; r++){
+        for (int c=0;c <r+1; c++){
+            cout << "*";
+        }
+        for (int c =0; c <2*n-2* r - 1; c++){ // row  + 1
+             cout << " ";
+        }
+        for (int c =0; c < r+1; c++){
+            cout << "*";
+        }
+        cout <<endl;
+        }
+    }
