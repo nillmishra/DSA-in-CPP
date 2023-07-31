f208b45d62d4d50bf27658b79272cfa5878eb2b1
Nill <nillmishra09@gmail.com>
2023-07-31 23:53:39 +0530

    decimal to binary by bitwise method

---
+++ number_system/decimal_binary_coversion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int decimalToBinary(int n) {
+    // Bitwise method
+    int binaryno = 0;
+    int i =0;
+    while (n > 0) {
+        int bit = (n & 1);
+        binaryno = bit * pow(10, i++) + binaryno;
+        n = n >> 1;
+    }
+    return binaryno;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int binary = decimalToBinary(n);
+    cout << binary << endl;
+}
