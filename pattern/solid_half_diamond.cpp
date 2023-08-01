677e3c001927fc32551f76eb401a4ccdc52f0204
Nill <nillmishra09@gmail.com>
2023-08-01 17:07:05 +0530

    inverted half dimond

---
+++ pattern/solid_half_diamond.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    //full pyramid
+    for(int r =0; r < n; r++){
+            for (int c =0; c < n -r -1;c++){ //n - r-  1 = n - (r + 1) 
+                cout << " ";
+            }
+            for (int c =0; c < r + 1; c++){ // row  + 1
+                cout << "*";
+            }
+        cout <<endl;
+        }  
+
+    for(int r =0; r < n; r++){
+            for (int c =0; c < r;c++){ //n - r-  1 = n - (r + 1) 
+                cout << " ";
+            }
+            for (int c =0; c < n-r; c++){ // row  + 1
+                cout <<  "*";
+            }
+            
+        cout <<endl;
+        }
+}
