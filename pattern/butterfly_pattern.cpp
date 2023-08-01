08b8d88ce29f1910c7c4407a32059e23e8f2a509
Nill <nillmishra09@gmail.com>
2023-08-01 17:04:06 +0530

    new type

---
+++ pattern/butterfly_pattern.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    //full pyramid
+    for(int r =0; r < n; r++){
+            for (int c =0; c < r + 1; c++){ // row  + 1
+                cout << "*";
+            }
+            for (int c =0; c < n-r; c++){ // row  + 1
+                cout <<  "  ";
+            }
+            for (int c =0; c < r + 1; c++){ // row  + 1
+                cout << "*";
+            }
+        cout <<endl;
+        }  
+
+    for(int r =0; r < n; r++){
+            for (int c =0; c < n-r; c++){ // row  + 1
+                cout <<  "*";
+            }
+            for (int c =0; c < r + 1; c++){ // row  + 1
+                cout << "  ";
+            }
+            for (int c =0; c < n-r; c++){ // row  + 1
+                cout <<  "*";
+            }
+            
+        cout <<endl;
+        }
+}
