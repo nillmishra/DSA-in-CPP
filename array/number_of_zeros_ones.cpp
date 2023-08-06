f98715367f628fd9585ad9885784bc501198e395
Nillotpal Mishra <nillmishra09@gmail.com>
2023-08-06 23:27:54 +0530

    zeros n ones

---
+++ array/number_of_zeros_ones.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[] = {1,0,0,1,0,1,0,1,0,1,1,0};
+    int size =12;
+    int numZero =0;
+    int numOne = 0;
+    
+    for(int i = 0; i< size; i++){
+        if(arr[i]==0){
+            numZero++;
+        }
+        if(arr[i]==1){
+            numOne++;
+        }
+    }
+    cout << "Number of Zero = "<< numZero << endl;
+    cout << "Number of One = "<< numOne << endl;
+
+}
