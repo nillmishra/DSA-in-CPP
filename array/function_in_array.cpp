0bae838c6b4229d13759e13239e0c60504a91545
Nillotpal Mishra <nillmishra09@gmail.com>
2023-08-06 23:25:41 +0530

    pass by refrence

---
+++ array/function_in_array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+ int printarray(int arr[], int size){
+    for(int i = 0; i< size; i++){
+        cout<< arr[i] << " ";
+    }
+    cout << endl;
+ }
+
+ int inc(int arr[], int size){
+    arr[0] = arr[0] + 10;
+    //actual array par operation hoo raha hai
+    printarray(arr, size);
+ }
+
+ int main(){
+    int arr[]={5,6};
+    int size =2;
+    inc (arr, size);
+    printarray(arr, size);
+ }
