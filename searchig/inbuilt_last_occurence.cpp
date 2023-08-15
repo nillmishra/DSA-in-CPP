b5d33867708a6c408a87bf33a2fe33567c508408
Nillotpal Mishra <nillmishra09@gmail.com>
2023-08-15 16:55:29 +0530

    inbuilt last occurence in binary search

---
+++ searchig/inbuilt_last_occurence.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int target = 4;
+    vector<int> v{1, 2, 4, 4, 4, 6, 7};
+    
+    auto upper = upper_bound(v.begin(), v.end(), target);
+    auto lastOccurrence = prev(upper);
+    
+    cout << "Index of last occurrence of " << target << ": " << lastOccurrence - v.begin() << endl;
+    
+    return 0;
+}
