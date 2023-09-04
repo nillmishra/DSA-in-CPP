6dfd5a9673a2b28d093b2208a346e87ed33c1d72
Nillotpal Mishra <nillmishra09@gmail.com>
2023-09-04 23:56:23 +0530

    k- closest by two pouinter aproach

---
+++ searchig/k-closet_elements.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector <int> twoPtrMethod(vector<int>& arr, int k, int x){
+ int l =0, h = arr.size() -1;
+ while(h-l>=k){
+    if(x - arr(l)> arr(h)-x){
+        l++;
+    }
+    else{
+        h--;
+    }
+    vector<int>ans;
+    for(int i =0; i<=h; i++){
+        ans.push_back(arr[i]);
+    }
+    return ans;
+ }
+}
+vector< int> findClosestElements(vector<int>& arr, int k, int x){
+    return twoPtrMethod(arr, k, x);
+}
