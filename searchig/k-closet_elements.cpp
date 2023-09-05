692bde251f6dab1bb93f2bc02e2a768f915be064
Nillotpal Mishra <nillmishra09@gmail.com>
2023-09-05 18:16:24 +0530

    k-closest element by two pointer method

--- searchig/k-closet_elements.cpp
+++ searchig/k-closet_elements.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
 #include <vector>
-using namespace std;
-vector <int> twoPtrMethod(vector<int>& arr, int k, int x){
- int l =0, h = arr.size() -1;
- while(h-l>=k){
-    if(x - arr(l)> arr(h)-x){
-        l++;
-    }
-    else{
-        h--;
-    }
-    vector<int>ans;
-    for(int i =0; i<=h; i++){
-        ans.push_back(arr[i]);
-    }
-    return ans;
+#include <algorithm> // For sorting
 
-    // using stl
-    //return vector<int>(arr.begin()+l, arr.begin()+h+1);
- }
+using namespace std;
+
+vector<int> twoPtrMethod(vector<int>& arr, int k, int x) {
+    int l = 0, h = arr.size() - 1;
+    while (h - l >= k) {
+        if (x - arr[l] > arr[h] - x) {
+            l++;
+        } else {
+            h--;
+        }
+    }
+
+    return vector<int>(arr.begin() + l, arr.begin() + h + 1);
 }
-vector< int> findClosestElements(vector<int>& arr, int k, int x){
+
+vector<int> findClosestElements(vector<int>& arr, int k, int x) {
     return twoPtrMethod(arr, k, x);
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int k = 3;
+    int x = 3;
+
+    vector<int> result = findClosestElements(arr, k, x);
+
+    cout << "Closest elements to " << x << " are:";
+    for (int num : result) {
+        cout << " " << num;
+    }
+    cout << endl;
+
+    return 0;
 }
