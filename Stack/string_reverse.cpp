7f09669a10c1243971d3a75ddbe8e7482b6bc559
Nillotpal Mishra <nillmishra09@gmail.com>
2023-12-19 20:49:58 +0530

    reverse of string

---
+++ Stack/string_reverse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include<stack>
+using namespace std;
+
+int main(){
+    string str = "Nillotpal";
+
+    stack<char>s;
+
+    for(int i = 0; i <str.length(); i++){
+        s.push(str[i]);
+    }
+
+
+    while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
+cout << endl;
+}
